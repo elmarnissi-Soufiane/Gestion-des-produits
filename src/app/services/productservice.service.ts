013bfe2d8e3a2ddbd07bc5a74c212bba02cdfa82
dib fatima zohra <dibfatimazohra501@gmail.com>
2024-12-31 16:07:26 +0100

    route

--- src/app/services/productservice.service.ts
+++ src/app/services/productservice.service.ts
@@ -11,9 +11,9 @@ export class ProductserviceService {
 
   // get all products
   getAllProducts(): Observable<Product[]> {
-    const host = 'http://localhost:3000';
+    // const host = 'http://localhost:3000';
     //const host = 'http://localhost:8000/api';
-    // const host = 'http://localhost:8000/api/v1';
+    const host = 'http://localhost:8000/api/v1';
     console.log(host);
     let data = this.http.get<Product[]>(host + '/products');
     console.log('products', data);
@@ -22,42 +22,42 @@ export class ProductserviceService {
 
   // Selected
   getSelectedProducts(): Observable<Product[]> {
-    const host = 'http://localhost:3000';
+    // const host = 'http://localhost:3000';
     //const host = 'http://localhost:8000/api';
-    // const host = 'http://localhost:8000/api/v1';
+    const host = 'http://localhost:8000/api/v1';
     return this.http.get<Product[]>(host + '/products?selected=true');
   }
 
   // Available
   getAvailableProducts(): Observable<Product[]> {
     //let host = environment.host;
-    const host = 'http://localhost:3000';
+    // const host = 'http://localhost:3000';
     //const host = 'http://localhost:8000/api';
-    // const host = 'http://localhost:8000/api/v1';
+    const host = 'http://localhost:8000/api/v1';
     return this.http.get<Product[]>(host + '/products?available=true');
   }
 
   // Search
   searchProduts(keyword: string): Observable<Product[]> {
-    const host = 'http://localhost:3000';
+    // const host = 'http://localhost:3000';
     //const host = 'http://localhost:8000/api';
-    // const host = 'http://localhost:8000/api/v1/products';
+    const host = 'http://localhost:8000/api/v1';
     return this.http.get<Product[]>(host + '/products?name=' + keyword);
   }
 
   // Ajouter un product
   saveProduct(product: Product): Observable<Product> {
-    const host = 'http://localhost:3000';
+    // const host = 'http://localhost:3000';
     //const host = 'http://localhost:8000/api';
-    // const host = 'http://localhost:8000/api/v1';
+    const host = 'http://localhost:8000/api/v1';
     return this.http.post<Product>(host + '/products', product);
   }
 
   // Delete
   deleteProdcut(product: Product): Observable<void> {
-    const host = 'http://localhost:3000';
+    // const host = 'http://localhost:3000';
     //const host = 'http://localhost:8000/api';
-    //const host = 'http://localhost:8000/api/v1';
+    const host = 'http://localhost:8000/api/v1';
     return this.http.delete<void>(`${host}/products/${product.id}`).pipe(
       catchError((error) => {
         console.error('Error deleting product:', error);
@@ -69,18 +69,18 @@ export class ProductserviceService {
   // get ById Edit
   getProductById(id: number): Observable<Product> {
     console.log('Fetching product with ID:', id); // Ajoutez ceci pour déboguer.
-    const host = 'http://localhost:3000';
+    // const host = 'http://localhost:3000';
     //const host = 'http://localhost:8000/api';
-    // const host = 'http://localhost:8000/api/v1';
+    const host = 'http://localhost:8000/api/v1';
     return this.http.get<Product>(`${host}/products/${id}`);
   }
 
   // Update
   updateProduct(product: Product): Observable<Product> {
     console.log('Updating product:', product); // Ajoutez ceci pour déboguer.
-    const host = 'http://localhost:3000';
+    // const host = 'http://localhost:3000';
     //const host = 'http://localhost:8000/api';
-    //const host = 'http://localhost:8000/api/v1';
+    const host = 'http://localhost:8000/api/v1';
     return this.http.put<Product>(`${host}/products/${product.id}`, product);
   }
 
@@ -89,7 +89,8 @@ export class ProductserviceService {
   onSelectProduct(product: Product): Observable<Product> {
     console.log('Product selected service', product);
 
-    const host = 'http://localhost:3000'; // Utilisez environment.apiUrl en production.
+    // const host = 'http://localhost:3000'; // Utilisez environment.apiUrl en production.
+    const host = 'http://localhost:8000/api/v1';
     const updatedProduct = { ...product, selected: !product.selected }; // Crée une copie et modifie "selected".
 
     return this.http
@@ -106,8 +107,9 @@ export class ProductserviceService {
   onAvailableProduct(product: Product): Observable<Product> {
     console.log('Available product service', product);
 
-    const host = 'http://localhost:3000'; // Recommandé : Utilisez des fichiers d'environnement pour stocker les URL.
+    // const host = 'http://localhost:3000'; // Recommandé : Utilisez des fichiers d'environnement pour stocker les URL.
 
+    const host = 'http://localhost:8000/api/v1';
     // Créer une copie du produit pour éviter de modifier un objet immuable
     const updatedProduct = { ...product, available: !product.available };
 
